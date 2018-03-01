be5f49130731f6f0dc84b88c08c06542c9a9f44f
Jozef Jarosciak <jarosciak@gmail.com>
2018-03-01 05:43:00 +0100

    Implemented color picker.

--- src/JavaPaintApp.java
+++ src/JavaPaintApp.java
@@ -1,14 +1,45 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class JavaPaintApp {
 
-    private JButton outlineColorButton;
-    private JButton fillColorButton;
+    private JButton selectOutlineColorButton;
+    private JButton selectFillColorButton;
     private JComboBox comboBox1;
     private JPanel mainpanel;
     private JPanel drawingPanel;
     private JToolBar toolbar;
+
+
+    public JavaPaintApp() {
+
+        selectOutlineColorButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Color initialBackground = selectOutlineColorButton.getBackground();
+                Color background = JColorChooser.showDialog(null,
+                    "JColorChooser Sample", initialBackground);
+                if (background != null) {
+                    selectOutlineColorButton.setBackground(background);
+                }
+            }
+        });
+
+        selectFillColorButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Color initialBackground = selectFillColorButton.getBackground();
+                Color background = JColorChooser.showDialog(null,
+                    "JColorChooser Sample", initialBackground);
+                if (background != null) {
+                    selectFillColorButton.setBackground(background);
+                }
+            }
+        });
+
+    }
 
     public static void main(String[] args) {
 
@@ -16,12 +47,14 @@ public class JavaPaintApp {
         JFrame frame = new JFrame("Java Paint Application)");
         frame.setContentPane(new JavaPaintApp().mainpanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 600);
+        frame.setSize(700, 700);
 
         frame.setVisible(true);
         // center on page
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setLocation(dim.width / 2 - frame.getSize().width / 2, dim.height / 2 - frame.getSize().height / 2);
+
+
 
     }
 
