87e45b0a9506f1e7d171c99de6ed8d3fa751fb0c
elvinazirima <36311611+elvinazirima@users.noreply.github.com>
2018-03-04 21:19:04 +0100

    Update
    Outline Color and shapes are now working. Check dashboard for a more detailed overview of this commit.
    
    Remaining - polymorphism implementation but thats a second to do, and fill color.

--- src/JavaPaintApp.java
+++ src/JavaPaintApp.java
@@ -15,6 +15,9 @@ public class JavaPaintApp extends JPanel {
     private JButton CLEARButton;
     private MyShape currentShape; //stores the current shape object
     private Graphics g;
+    Point startPoint, endPoint;
+    String selectedShape = "Line";
+    Color fillColor, outlineColor;
 
 
     public JavaPaintApp() {
@@ -24,10 +27,10 @@ public class JavaPaintApp extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Color initialBackground = selectOutlineColorButton.getBackground();
-                Color background = JColorChooser.showDialog(null,
-                    "Shape Outline Color Picker", initialBackground);
-                if (background != null) {
-                    selectOutlineColorButton.setBackground(background);
+                outlineColor = JColorChooser.showDialog(null,
+                        "Shape Outline Color Picker", initialBackground);
+                if (outlineColor != null) {
+                    selectOutlineColorButton.setBackground(outlineColor);
                 }
             }
         });
@@ -36,31 +39,41 @@ public class JavaPaintApp extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Color initialBackground = selectFillColorButton.getBackground();
-                Color background = JColorChooser.showDialog(null,
-                    "Shape Fill Color Picker", initialBackground);
-                if (background != null) {
-                    selectFillColorButton.setBackground(background);
+                fillColor = JColorChooser.showDialog(null,
+                        "Shape Fill Color Picker", initialBackground);
+                if (fillColor != null) {
+                    selectFillColorButton.setBackground(fillColor);
                 }
             }
         });
+        comboBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ItemSelectable is = (ItemSelectable)e.getSource();
+                System.out.println("Cmd: "+ e.getActionCommand());
+                System.out.println(selectedString(is));
+                selectedShape = selectedString(is);
+            }
+        });
+
 
         drawingPanel.addMouseMotionListener(new MouseMotionAdapter() {
             @Override
             public void mouseMoved(MouseEvent e) {
                 super.mouseMoved(e);
                 mouseCoordinates.setText("Mouse Coordinates: " + e.getX() + ", " + e.getY());
-                currentShape = new MyRectangle( e.getX(), e.getY(),e.getX(), e.getY(), Color.green);
-                currentShape.setCoordinate2(e.getX());
-                currentShape.setcoordinate4(e.getY());
-                //currentShape.drawshape(drawingPanel.getGraphics());
-                //drawingPanel.repaint();
-                drawingPanel.getGraphics().drawRect(e.getX(), e.getY(),e.getX(), e.getY());
+
             }
 
             @Override
             public void mouseDragged(MouseEvent e) {
                 super.mouseDragged(e);
                 System.out.println("mouseDragged: " + e.getX() + ", " + e.getY());
+                currentShape = new MyRectangle( e.getX(), e.getY(),e.getX(), e.getY(), Color.green);
+                currentShape.setCoordinate2(e.getX());
+                currentShape.setcoordinate4(e.getY());
+                //currentShape.drawshape(drawingPanel.getGraphics());
+                //drawingPanel.repaint();
             }
         });
 
@@ -69,12 +82,17 @@ public class JavaPaintApp extends JPanel {
             @Override
             public void mouseReleased(MouseEvent e) {
                 super.mouseReleased(e);
+                endPoint = e.getPoint();
+                //drawingPanel.getGraphics().drawRect((int)startPoint.getX(), (int)startPoint.getY(), (int)endPoint.getX(), (int) endPoint.getY());
+
+                drawShape(selectedShape);
                 System.out.println("mouseReleased: " + e.getX() + ", " + e.getY());
             }
 
             @Override
             public void mousePressed(MouseEvent e) {
                 super.mousePressed(e);
+                startPoint = e.getPoint();
                 System.out.println("mousePressed: " + e.getX() + ", " + e.getY());
 
             }
@@ -91,6 +109,32 @@ public class JavaPaintApp extends JPanel {
         }
     }
 */
+    private String selectedString(ItemSelectable is) {
+        Object selected[] = is.getSelectedObjects();
+        return ((selected.length == 0) ? "null" : (String)selected[0]);
+    }
+
+    private void drawShape(String shape){
+        Graphics graphics = drawingPanel.getGraphics();
+        graphics.setColor(outlineColor);
+        switch (shape){
+            case "Line":
+                graphics.drawLine((int)startPoint.getX(), (int)startPoint.getY(), (int)endPoint.getX(), (int) endPoint.getY());
+                break;
+            case "Rectangle":
+                graphics.drawRect((int)startPoint.getX(), (int)startPoint.getY(), (int)endPoint.getX(), (int) endPoint.getY());
+                break;
+            case "Oval":
+                graphics.drawOval((int)startPoint.getX(), (int)startPoint.getY(), (int)endPoint.getX(), (int) endPoint.getY());
+                break;
+            case "Circle":
+                graphics.drawOval((int)startPoint.getX(), (int)startPoint.getY(), (int)endPoint.getX(), (int) endPoint.getY());
+                break;
+            default:
+                drawingPanel.getGraphics().drawLine((int)startPoint.getX(), (int)startPoint.getY(), (int)endPoint.getX(), (int) endPoint.getY());
+                break;
+        }
+    }
 
     public static void main(String[] args) {
 
